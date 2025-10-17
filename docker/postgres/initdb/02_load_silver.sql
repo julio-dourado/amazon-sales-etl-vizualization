5c332043e5a75689bc8553a77bdad2eef7804835
Gustavo Henrique <gustavohenriqueprivado@gmail.com>
2025-10-16 21:04:58 -0300

    fix: corrigindo path do bronze e colunas com dados limpos da silver

--- docker/postgres/initdb/02_load_silver.sql
+++ docker/postgres/initdb/02_load_silver.sql
@@ -3,20 +3,17 @@ SET client_encoding TO 'UTF8';
 COPY silver.amazon_products_sales_curated (
     title,
     rating,
-    number_of_reviews,
-    bought_in_last_month,
-    "current/discounted_price",
-    price_on_variant,
-    listed_price,
+    total_reviews,
+    purchased_last_month,
+    discounted_price,
+    original_price,
     is_best_seller,
     is_sponsored,
-    is_couponed,
+    has_coupon,
     buy_box_availability,
-    delivery_details,
-    sustainability_badges,
-    image_url,
-    product_url,
-    collected_at
+    date,
+    time,
+    coupon_discount_pct
 )
 FROM '/data/silver/amazon_products_cleaned.csv'
 WITH (
